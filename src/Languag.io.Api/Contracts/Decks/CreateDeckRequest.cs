5e26fe3f49e5ea34a951d8ca16364e29eb97bfbe
Emilio F <emiliofurrer00@gmail.com>
2025-11-17 00:18:28 +0100

    add contract for CreateDeckRequest

---
+++ src/Languag.io.Api/Contracts/Decks/CreateDeckRequest.cs
@@ -0,0 +1,11 @@
+﻿using Languag.io.Domain.Entities;
+
+namespace Languag.io.Api.Contracts.Decks;
+
+public class CreateDeckRequest
+{
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string? LanguageCode { get; set; }
+    public DeckVisibility Visibility { get; set; } = DeckVisibility.Private;
+}
