01590f7b23dbf4412258e52c86428852147c9ac8
Emilio F <emiliofurrer00@gmail.com>
2026-02-10 07:54:11 +0100

    add migration

---
+++ src/Languag.io.Infrastructure/Migrations/20260210004557_UpdateDeckModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Languag.io.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDeckModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LanguageCode",
+                table: "Decks",
+                newName: "Color");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Decks",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Decks");
+
+            migrationBuilder.RenameColumn(
+                name: "Color",
+                table: "Decks",
+                newName: "LanguageCode");
+        }
+    }
+}
