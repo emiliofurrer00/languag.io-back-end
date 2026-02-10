0ad8cc945cf4293b421edd22952118cb74617b6c
Emilio F <emiliofurrer00@gmail.com>
2026-02-10 07:54:26 +0100

    add Response domain entity

---
+++ src/Languag.io.Domain/Entities/Response.cs
@@ -0,0 +1,12 @@
+﻿namespace Languag.io.Domain.Entities
+{
+    public class Response
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid GameId { get; set; }
+        public bool IsCorrect { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime UpdatedAtUtc { get; set; }
+    }
+}
