2af1c381cc50139bc10c255b0d33b0cd607d7908
Emilio F <emiliofurrer00@gmail.com>
2026-02-26 18:47:39 +0100

    make ownerId column nullable in Decks table

--- src/Languag.io.Domain/Entities/Deck.cs
+++ src/Languag.io.Domain/Entities/Deck.cs
@@ -7,7 +7,7 @@ namespace Languag.io.Domain.Entities
     public class Deck
     {
         public Guid Id { get; set; }
-        public Guid OwnerId { get; set; }
+        public Guid? OwnerId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string? Category { get; set; }
---
+++ src/Languag.io.Infrastructure/Migrations/20260226174536_MakeDeckOwnerIdNullable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Languag.io.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeDeckOwnerIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OwnerId",
+                table: "Decks",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.Sql("""
+                UPDATE "Decks"
+                SET "OwnerId" = NULL
+                WHERE "OwnerId" IS NOT NULL;
+             """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OwnerId",
+                table: "Decks",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+        }
+    }
+}
