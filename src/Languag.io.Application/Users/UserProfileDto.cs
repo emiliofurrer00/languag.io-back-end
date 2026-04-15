50e5d9cdf55be2e4307c0a91b77ed26e8bf1b521
Emilio F <emiliofurrer00@gmail.com>
2026-04-15 02:27:48 +0200

    extend UserProfileDto

--- src/Languag.io.Application/Users/UserProfileDto.cs
+++ src/Languag.io.Application/Users/UserProfileDto.cs
@@ -3,8 +3,11 @@ namespace Languag.io.Application.Users;
 public sealed record UserProfileDto(
     Guid Id,
     string ExternalId,
+    string? Username,
     string? Name,
     string? Email,
+    bool HasBeenOnboarded,
+    int DailyCardsGoal,
     string ProfileDescription,
     string About,
     bool IsPublicProfile);
